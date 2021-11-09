5b4cb0bf02b8280d4c2005a53520c42c0075819e
carljhn <zoletacarljohn1@gmail.com>
2021-11-08 14:15:31 -1200

    Second commit nameAgeAddv3

--- nameAgeAddv3.py
+++ nameAgeAddv3.py
@@ -4,3 +4,5 @@ def getnameAgeAddress():
     address_=input("Enter your address: ")
     return name_, age_, address_
 
+def display(name1, age1, address1):
+    print(f"Hi, my name is {name1}. I am {age1} years old and I live in {address1}.")
