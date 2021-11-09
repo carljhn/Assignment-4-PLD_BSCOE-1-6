72ea6b9812cfb69614759397b4e07135b3aac35f
carljhn <zoletacarljohn1@gmail.com>
2021-11-08 14:18:18 -1200

    Final commit nameAgeAddv3

--- nameAgeAddv3.py
+++ nameAgeAddv3.py
@@ -6,3 +6,6 @@ def getnameAgeAddress():
 
 def display(name1, age1, address1):
     print(f"Hi, my name is {name1}. I am {age1} years old and I live in {address1}.")
+
+name, age, address= getnameAgeAddress()
+display(name, age, address)
